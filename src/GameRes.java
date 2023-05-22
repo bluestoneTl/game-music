ff9969c00de9017eaad05b927dfaba941cd6486c
bluestone <2055325983@qq.com>
2023-05-22 10:43:45 +0800

    修正音乐路径

--- src/GameRes.java
+++ src/GameRes.java
@@ -7,15 +7,15 @@ public class GameRes {
     //资源加载模块
     //郭西达测试
     //perfect组件
-    public static JLabel pecfectIcon = new JLabel(new ImageIcon());
+    public static JLabel pecfectIcon = new JLabel(new ImageIcon("src/resource/image/perfect.png"));
     //good组件
-    public static JLabel goodIcon = new JLabel(new ImageIcon());
+    public static JLabel goodIcon = new JLabel(new ImageIcon("src/resource/image/good.png"));
     //miss 组件
-    public static JLabel missIcon = new JLabel(new ImageIcon());
+    public static JLabel missIcon = new JLabel(new ImageIcon("src/resource/image/miss.png"));
     //rail : 轨道图片组件
-    static JLabel rail =new JLabel(new ImageIcon());
+    static JLabel rail =new JLabel(new ImageIcon("src/resource/image/rail.png"));
     // background : 背景图片组件
-    static JLabel background = new JLabel(new ImageIcon());
+    static JLabel background = new JLabel(new ImageIcon("src/resource/image/livebg.png"));
      //perfectSound : perfect音效，用数组避免间隔过短无法完全播放
     public static AudioClip[] perfectSound = new AudioClip[3];
    // goodSound : good音效，用数组避免间隔过短无法完全播放
@@ -31,15 +31,15 @@ public class GameRes {
    
     public static void load(Note[] notes) {
         try {
-            URL pecfectUrl = new File("").toURI().toURL();
-            URL goodUrl = new File("").toURI().toURL();
+            URL pecfectUrl = new File("src/resource/music/perfecthit.wav").toURI().toURL();
+            URL goodUrl = new File("src/resource/music/goodhit.wav").toURI().toURL();
             perfectSound[0] = Applet.newAudioClip(pecfectUrl);
             perfectSound[1] = Applet.newAudioClip(pecfectUrl);
             perfectSound[2] = Applet.newAudioClip(pecfectUrl);
             goodSound[0] = Applet.newAudioClip(goodUrl);
             goodSound[1] = Applet.newAudioClip(goodUrl);
             goodSound[2] = Applet.newAudioClip(goodUrl);
-            resultbgm = Applet.newAudioClip(new File("").toURI().toURL());
+            resultbgm = Applet.newAudioClip(new File("src/resource/music/resultbgm.wav").toURI().toURL());
         } catch (Exception e) {
         }
 
