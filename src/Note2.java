35f7eb174fbcdde13d2749396b44453eb251a764
01 <2210942903@qq.com>
2023-05-28 20:55:11 +0800

    更新图片

--- src/Note2.java
+++ src/Note2.java
@@ -1,4 +1,17 @@
+import javax.swing.*;
+public class Note2 extends JLabel {
+    //音符类
+//图片资源
+    static ImageIcon noteIcon = new ImageIcon("src/resource/image/note.png");
+    //定义横坐标
+    public int x;
+    //定义纵坐标
+    public int y;
+    //构造函数
+    public Note2() {
+        this.setIcon(noteIcon);
+        this.setSize(120, 18);
+        this.setVisible(false);
+    }
 
-public class Note2 {
-    //背景动效音符类
 }
